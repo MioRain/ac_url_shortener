670b8e3bc07e9312cf40baa0a2ea6d790066a8e1
MioRain <ericzero1593@hotmail.com>
2022-05-31 15:34:30 +0800

    feat: init project

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express server is running on http://localhost:${port}`)
+})
